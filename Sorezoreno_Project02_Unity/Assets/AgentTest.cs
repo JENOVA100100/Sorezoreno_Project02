dfa931ce0f3cde635202b2623320332fa8b63f04
JENOVA100100 <v7defbbdf7769@gmail.com>
2021-01-21 01:50:06 +0900

    playerから逃げるCapsuleを実装しました！

---
+++ Sorezoreno_Project02_Unity/Assets/AgentTest.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AgentTest : MonoBehaviour
+{
+    public NavMeshAgent me;
+    public Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        me = gameObject.GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        me.destination = 2 * transform.position - target.position;
+
+        float dist = Vector3.Distance(target.position, transform.position);
+
+        if(dist > 10f)
+        {
+            me.speed = 0;
+        }
+        else if(dist < 5f)
+        {
+            me.speed = 10f;
+        }
+        else
+        {
+            me.speed = 3.5f;
+        }
+
+
+    }
+}
